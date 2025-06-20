9f2cc3c5b305ddedd858c2842e1ac0ca3bd3fb67
Kayky Rodrigues <rodrigueskayky2025@gmail.com>
2025-06-20 09:24:11 -0300

    Pequenos ajustes no scrip e no estilo

--- weatherapi.js
+++ weatherapi.js
@@ -9,6 +9,7 @@ function pressEnter(event) {
 
         function getApi() {
 
+            let region = document.getElementById('region')
             let localTime = document.getElementById('localTime')
             let temp = document.getElementById('temp')
 
@@ -16,6 +17,7 @@ function pressEnter(event) {
             fetch(`https://api.weatherapi.com/v1/current.json?key=70befa9fc4dc4fe0a3c140404251006&q=${loc}&aqi=no`)
                 .then(response => response.json())
                 .then(data => {
+                    region.innerHTML = data.location.region
                     localTime.innerHTML = data.location.localtime
                     temp.innerHTML = data.current.temp_c + '°'
                 })
