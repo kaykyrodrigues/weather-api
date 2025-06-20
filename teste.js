494e56875b72d9da3e074db5ef0bfc0f89e82922
Kayky Rodrigues <rodrigueskayky2025@gmail.com>
2025-06-20 10:47:41 -0300

    Testes para a API

--- teste.js
+++ teste.js
@@ -1,7 +1,7 @@
 function getApi() {
 
     const key = '70befa9fc4dc4fe0a3c140404251006'
-    fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=London&aqi=no`)
+    fetch(`http://api.weatherapi.com/v1/current.json?key=${key}&q=Fortaleza&aqi=no`)
         .then(response => response.json())
         .then(data => {
             console.log(data)
