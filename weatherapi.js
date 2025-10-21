d533a6cb8fa3fec782eec5830632bd8e450d336b
Kayky <rodrigueskayky2025@gmail.com>
2025-10-21 08:59:17 -0300

    Atualizando o método de consumo da API

--- weatherapi.js
+++ weatherapi.js
@@ -1,35 +1,43 @@
-document.getElementById('txtloc')
-    .addEventListener('keydown', pressEnter)
+document.getElementById("txtloc").addEventListener("keydown", pressEnter);
 
 function pressEnter(event) {
+  if (event.key === "Enter") {
+    let loc = event.target.value;
+    console.log(loc);
 
-    if (event.key === 'Enter') {
-        let loc = event.target.value
-        console.log(loc)
+    async function getApi() {
+      const region = document.getElementById("region");
+      const localTime = document.getElementById("localTime");
+      const img = document.getElementById("img");
+      const temp = document.getElementById("temp");
 
-        function getApi() {
+      try {
+        const apiKey = "70befa9fc4dc4fe0a3c140404251006";
+        const response = await fetch(
+          `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=$=${loc}&aqi=no`
+        );
 
-            let region = document.getElementById('region')
-            let localTime = document.getElementById('localTime')
-            let img = document.getElementById('img')
-            let temp = document.getElementById('temp')
-
-            const key = '70befa9fc4dc4fe0a3c140404251006'
-            fetch(`https://api.weatherapi.com/v1/current.json?key=70befa9fc4dc4fe0a3c140404251006&q=${loc}&aqi=no`)
-                .then(response => response.json())
-                .then(data => {
-                    region.innerHTML = `<img src="images/locationpoint.png" alt="Ponto de localização" id="locationPoint">` + data.location.region + ', ' + data.location.country;
-                    localTime.innerHTML = data.location.localtime;
-                    img.src = data.current.condition.icon;
-                    temp.innerHTML = data.current.temp_c + '°';
-                    condition.innerHTML = 'Time condition: ' + data.current.condition.text; '.'
-                })
-                .catch(err => {
-                    console.log('Error: ' + err)
-                })
+        if (!response.ok) {
+          throw new Error(
+            `Erro na API: ${response.status} ${response.statusText}`
+          );
         }
 
-        getApi()
-    }
-}
+        const data = await response.json();
 
+        region.innerHTML = `
+            <img src="images/locationpoint.png" alt="Ponto de localização" id="locationPoint">
+            ${data.location.region}, ${data.location.country}
+            `;
+        localTime.textContent = data.location.localtime;
+        img.src = data.current.condition.icon;
+        temp.textContent = `${data.current.temp_c}°`;
+        condition.textContent = `Time condition: ${data.current.condition.text}`;
+      } catch (err) {
+        console.error("Erro ao capturar dados: " + err.message);
+      }
+    }
+
+    getApi();
+  }
+}
